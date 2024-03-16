43ae96d1166be0794314256e8c604da8bfc23188
Aryan Patel <144789919+aryanptel@users.noreply.github.com>
2024-03-16 11:14:19 +0530

    Update Arming_Takeoff.py

--- UAV Drones Swarming/Arming_Takeoff.py
+++ UAV Drones Swarming/Arming_Takeoff.py
@@ -1,14 +1,17 @@
+#In this code, we are connecting three drones using using ESP8266 through a single router. We are using multi-threading to control multiple drones at the same time.
 from pymavlink import mavutil
 import threading
 import time
 
+#Checking the hearbeat of the drones.
 def connect_to_mavproxy(address, port):
     mav = mavutil.mavlink_connection(f'udpin:{address}:{port}')
     mav.wait_heartbeat()
     print(f"Heartbeat from the system (system {mav.target_system}, component {mav.target_component})")
     return mav
 
-def perform_flight_operations(mav):
+#mode to guided -> arming -> throttle and takeoff to 7m height
+def flight_operations(mav):
     print("Mode")
     mav.mav.set_mode_send(mav.target_system, mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, 4)
     mode_ack = mav.recv_match(type='COMMAND_ACK', blocking=True)
@@ -31,18 +34,18 @@ def perform_flight_operations(mav):
 
     time.sleep(1)
 
-# Define the addresses and ports for each connection
+# addresses and ports of each drone.
 connections = [('127.0.0.1', 15560), ('127.0.0.1', 15570), ('127.0.0.1', 15590)]
 
-# Create and start a thread for each connection
+#Multi-Threading
 threads = []
 for address, port in connections:
     mav = connect_to_mavproxy(address, port)
-    thread = threading.Thread(target=perform_flight_operations, args=(mav,))
+    thread = threading.Thread(target=flight_operations, args=(mav,))
     thread.start()
     threads.append(thread)
 
-# Wait for all threads to finish
+# executing all at the same time.
 for thread in threads:
     thread.join()
 
